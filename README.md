35bc01f04f524243f98033de0cb0e92a85754384
handyPan <courseware.cs@gmail.com>
2023-01-28 19:51:18 -0500

    ex007-color-palette completed

--- README.md
+++ README.md
@@ -15,3 +15,4 @@ For video demonstration, please visit the project website at <a href="https://ha
 [Exercise 004 - Todo List](https://handypan.github.io/Vanilla-JavaScript-Exercise-Collection/ex004-todo-list/)<br>
 [Exercise 005 - Multiple Theme](https://handypan.github.io/Vanilla-JavaScript-Exercise-Collection/ex005-multiple-theme/)<br>
 [Exercise 006 - File Uploader](https://handypan.github.io/Vanilla-JavaScript-Exercise-Collection/ex006-file-uploader/)<br>
+[Exercise 007 - Color Palette](https://handypan.github.io/Vanilla-JavaScript-Exercise-Collection/ex007-color-palette/)<br>
---
+++ ex007-color-palette/main.js
@@ -0,0 +1,34 @@
+const container = document.querySelector(".container");
+const refreshBtn = document.querySelector(".refresh-btn");
+
+const maxPaletteBoxes = 32;
+
+const generatePalette = () => {
+    container.innerHTML = "";
+    for (let i = 0; i < maxPaletteBoxes; i++) {
+        let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+        randomHex = `#${randomHex.padStart(6, '0')}`;
+        const color = document.createElement("li");
+        color.classList.add("color");
+        color.innerHTML = `
+            <div class="rect-box" style="background: ${randomHex}"></div>
+            <span class="hex-value">${randomHex}</span>
+            `;
+        color.addEventListener('click', () => copyColor(color, randomHex));
+        container.appendChild(color);
+    }
+};
+
+const copyColor = (elem, hexVal) => {
+    const colorElement = elem.querySelector(".hex-value");
+    navigator.clipboard.writeText(hexVal).then(() => {
+        colorElement.innerText = "Copied";
+        setTimeout(() => colorElement.innerText = hexVal, 1000);
+    }).catch(() => alert("Failed to copy the color code!"));
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+    generatePalette();
+});
+
+refreshBtn.addEventListener('click', generatePalette);
